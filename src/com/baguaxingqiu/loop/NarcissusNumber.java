37ad47ce7faef8c5f63c6b7e4c40979501af2cc8
xiongleiwinnie <945058495@qq.com>
2017-06-12 00:48:12 +0800

    水仙花数

--- src/com/baguaxingqiu/loop/NarcissusNumber.java
+++ src/com/baguaxingqiu/loop/NarcissusNumber.java
@@ -10,9 +10,19 @@ public class NarcissusNumber {
 		寻找所有的水仙花数
 	 */
 	public static void main(String[] args) {
-		int a;
-		int b;
-		int c;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				for (int k = 0; k < 10; k++) {
+					if (i*i*i+j*j*j+k*k*k == i*100+k*10+j) {
+						int l = i*100+k*10+j;
+						if (l > 99) {
+							System.out.println(l);
+						}
+						
+					}
+				}
+			}
+		}
 
 	}
 
