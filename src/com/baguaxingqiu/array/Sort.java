7f935be35f5873e9c8308917dc6b81ffb154dcdb
xiongleiwinnie <945058495@qq.com>
2017-06-13 00:00:28 +0800

    数组排序

---
+++ src/com/baguaxingqiu/array/Sort.java
@@ -0,0 +1,16 @@
+package com.baguaxingqiu.array;
+
+public class Sort {
+
+	/**
+	 * @param args
+	 *  首先创建一个长度是5的数组,并填充随机数。 
+		首先用选择法正排序，然后再对其使用冒泡法倒排序
+	 */
+	public static void main(String[] args) {
+		int a[] = new int[5];
+		a[0] = (int)(Math.random()*100);
+
+	}
+
+}
