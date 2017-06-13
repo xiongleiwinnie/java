fc5aee73106c3e057dfa02c0ef710180118cd1cc
xiongleiwinnie <945058495@qq.com>
2017-06-13 17:14:19 +0800

    数组冒泡、选择排序

--- src/com/baguaxingqiu/array/Sort.java
+++ src/com/baguaxingqiu/array/Sort.java
@@ -9,7 +9,39 @@ public class Sort {
 	 */
 	public static void main(String[] args) {
 		int a[] = new int[5];
-		a[0] = (int)(Math.random()*100);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (int)(Math.random()*100);
+			System.out.println(a[i]);
+		}
+		
+		//Ñ¡Ôñ·¨
+		for (int i = 0; i < a.length-1; i++) {
+			for (int j = i+1; j < a.length; j++) {
+				if (a[i]>a[j]) {
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				};
+			}
+		}
+		
+		//Ã°ÅÝ·¨
+//		for (int i = 0; i < a.length; i++) {
+//			for (int j = 0; j < a.length-i-1; j++) {
+//				if (a[i]<a[i+1]) {
+//					int temp = a[i];
+//					a[i] = a[i+1];
+//					a[i+1] = temp;
+//				}
+//			}
+//		}
+		
+		
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+		
 
 	}
 
