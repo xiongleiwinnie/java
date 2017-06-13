a4da845b2034b260a81ea722d3e41d8b8186c0f0
xiongleiwinnie <945058495@qq.com>
2017-06-13 21:40:35 +0800

    增强型for循环

--- src/com/baguaxingqiu/array/StrengthenForLoop.java
+++ src/com/baguaxingqiu/array/StrengthenForLoop.java
@@ -7,7 +7,18 @@ public class StrengthenForLoop {
 	 * 用增强型for循环找出最大的那个数
 	 */
 	public static void main(String[] args) {
-		
+		int a[] = new int[5];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = (int)(Math.random()*100);
+			System.out.println(a[i]);
+		}
+		int b = 0;
+		for(int each:a){
+			if (b<each) {
+				b = each;
+			}
+		}
+		System.out.println("最大数为："+b);
 		
 	}
 
