6c7c060b24fa33e5e93b571500f7e2c19de7d114
xiongleiwinnie <945058495@qq.com>
2017-06-14 23:08:16 +0800

    英雄类

---
+++ src/com/baguaxingqiu/classandobject/Hero.java
@@ -0,0 +1,22 @@
+package com.baguaxingqiu.classandobject;
+
+public class Hero {
+    
+    String name; //姓名
+      
+    float hp; //血量
+      
+    float armor; //护甲
+      
+    int moveSpeed; //移动速度
+      
+    public static void main(String[] args) {
+        //创建一个对象
+        new Hero();
+         
+        //使用一个引用来指向这个对象
+        Hero h = new Hero();
+         
+    }   
+      
+}
