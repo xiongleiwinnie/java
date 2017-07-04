519014881fa2fbccca039505a451d7c9eef51319
xiongleiwinnie <945058495@qq.com>
2017-07-04 17:48:43 +0800

    比较

---
+++ src/com/baguaxingqiu/numberandstring/Compare.java
@@ -0,0 +1,26 @@
+package com.baguaxingqiu.numberandstring;
+
+import javax.xml.stream.events.EndDocument;
+
+public class Compare {
+
+	/**
+	 * @param args
+	 *  创建一个长度是100的字符串数组
+		使用长度是2的随机字符填充该字符串数组
+		统计这个字符串数组里重复的字符串有多少种
+	 */
+	public static void main(String[] args) {
+		String[] s = new String[100];
+		for (String string : s) {
+			short start = 0;
+			short end = 'z'+1;
+			char a = (char)(Math.random()*(end-start)+start);
+			char b = (char)(Math.random()*(end-start)+start);
+			s = String()+String();
+		}
+		
+
+	}
+
+}
