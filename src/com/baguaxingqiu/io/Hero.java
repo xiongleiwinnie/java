4a44b48283855a064a7bee92a5a2e2e04731eee5
xiongleiwinnie <945058495@qq.com>
2017-09-05 17:33:26 +0800

    序列化

---
+++ src/com/baguaxingqiu/io/Hero.java
@@ -0,0 +1,18 @@
+package com.baguaxingqiu.io;
+
+import java.io.Serializable;
+
+public class Hero implements Serializable{
+	private static final long serialVersionUID = 1L; 
+    public String name;
+    public float hp;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/baguaxingqiu/io/Serialize.java
@@ -0,0 +1,16 @@
+package com.baguaxingqiu.io;
+
+public class Serialize {
+
+	/**
+	 * @param args
+	 * 准备一个长度是10，类型是Hero的数组，使用10个Hero对象初始化该数组
+                       然后把该数组序列化到一个文件heros.lol
+                       接着使用ObjectInputStream 读取该文件，并转换为Hero数组，验证该数组中的内容，是否和序列化之前一样
+	 */
+	public static void main(String[] args) {
+		Hero[] heros = new Hero[10];
+
+	}
+
+}
